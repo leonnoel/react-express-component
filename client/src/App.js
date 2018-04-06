2759514677a772d107f7c43baa39f513d4f192c6
Leon Noel <leon.a.noel+github@gmail.com>
2018-04-06 10:33:42 -0400

    added folder for component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Messages from './Messages.js';
+import Messages from './Messages/Messages.js';
 
 class App extends Component {
 
